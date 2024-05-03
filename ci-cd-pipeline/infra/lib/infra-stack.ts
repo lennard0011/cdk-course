26564ca2e9c39c25ea19a4d5ab1c7f68bf192765
Lennard van der Plas <lennardplas@gmail.com>
2024-05-03 13:45:48 +0200

    add cicd pipeline

---
+++ ci-cd-pipeline/infra/lib/infra-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Construct } from 'constructs';
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('lennard0011/cdk-course', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+    })
+
+    // Add stages to the pipeline
+
+  }
+}
