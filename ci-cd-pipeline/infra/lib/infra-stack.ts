687a6f38fde9c40509618f0199ac16e7b1e349c2
Lennard van der Plas <lennardplas@gmail.com>
2024-05-03 13:54:52 +0200

    command to relevant folder

--- ci-cd-pipeline/infra/lib/infra-stack.ts
+++ ci-cd-pipeline/infra/lib/infra-stack.ts
@@ -10,6 +10,7 @@ export class InfraStack extends cdk.Stack {
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('lennard0011/cdk-course', 'main'),
         commands: [
+          'cd ci-cd-pipeline/infra',
           'npm ci',
           'npm run build',
           'npx cdk synth',
