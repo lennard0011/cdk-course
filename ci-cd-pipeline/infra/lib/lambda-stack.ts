67ded35630bdf0f8bd7f891500c6866ce1941bf9
Lennard van der Plas <lennardplas@gmail.com>
2024-05-03 14:31:50 +0200

    add simple lambda function

---
+++ ci-cd-pipeline/infra/lib/lambda-stack.ts
@@ -0,0 +1,15 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+
+export class lambdaStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const lambda = new Function(this, 'Lambda', {
+            runtime: Runtime.NODEJS_20_X,
+            handler: 'index.handler',
+            code: Code.fromInline('exports.handler = async function(event) { return "hello world"; }'),
+        });
+    }
+}
